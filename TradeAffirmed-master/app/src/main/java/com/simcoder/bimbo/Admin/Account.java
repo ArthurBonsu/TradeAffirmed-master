eb68f8f9a845d708c4cfa5811aaee0d1d812c964
ArthurBonsu <peeman34@gmail.com>
2021-02-13 01:11:37 +0800

    SETTING UP ACTIVITIES

--- TradeAffirmed-master/app/src/main/java/com/simcoder/bimbo/Admin/Account.java
+++ TradeAffirmed-master/app/src/main/java/com/simcoder/bimbo/Admin/Account.java
@@ -1,9 +1,20 @@
 package com.simcoder.bimbo.Admin;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.simcoder.bimbo.R;
 
 public class Account extends AppCompatActivity {
     public Account() {
         super();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.accountbackground);
     }
 }
