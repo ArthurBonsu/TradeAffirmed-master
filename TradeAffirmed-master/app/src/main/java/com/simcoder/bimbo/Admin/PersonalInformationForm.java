1001086a6f1a597a100e6226bb282169d20113b4
ArthurBonsu <peeman34@gmail.com>
2021-04-04 08:01:04 +0800

    STRINGS ATTACHED
    Added the country strings

--- TradeAffirmed-master/app/src/main/java/com/simcoder/bimbo/Admin/PersonalInformationForm.java
+++ TradeAffirmed-master/app/src/main/java/com/simcoder/bimbo/Admin/PersonalInformationForm.java
@@ -6,6 +6,7 @@ import com.google.firebase.auth.FirebaseUser;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.net.Uri;
 import android.provider.MediaStore;
 import androidx.annotation.NonNull;
@@ -16,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -82,6 +84,27 @@ public class PersonalInformationForm extends AppCompatActivity {
     String thenameinfostring;
     String theemailinfostring;
     String thephoneinfostring;
+    String agetext;
+
+
+    EditText NameofPerson;
+    EditText  PhoneNumberOfPerson;
+    EditText PersonEmail;
+    RadioGroup genderspinnerradiogroup;
+    RadioButton MaleRadiobutton;
+
+    RadioButton FemaleMaleRadiobutton;
+    Spinner agespinner;
+    EditText NationalIDofEmergencyPerson;
+    Spinner countryspinner;
+    Button saveinformationhere;
+    ImageButton movetonext;
+    ImageButton homebutton;
+    ImageButton suggestionsbutton;
+     ImageButton       services;
+     ImageButton  expectedshipping;
+     ImageButton       adminprofile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,24 +120,36 @@ public class PersonalInformationForm extends AppCompatActivity {
             traderID = traderIDintent.getExtras().getString("traderID");
         }
 
-        Nameinfo = findViewById(R.id.Nameinfo);
-        Emailinfo = findViewById(R.id.Phoneinfo);
-        Phoneinfo = findViewById(R.id.Emailinfo);
+         NameofPerson = findViewById(R.id.NameofPerson);
+          PhoneNumberOfPerson = findViewById(R.id.PhoneNumberOfPerson);
+         PersonEmail = findViewById(R.id.PersonEmail);
 
-
-        NameinfoString = Nameinfo.getText().toString();
-        EmailinfoString = Emailinfo.getText().toString();
-        PhoneinfoString = Phoneinfo.getText().toString();
+        NameofPersonString = NameofPerson.getText().toString();
+        PersonEmailString  = PersonEmail.getText().toString();
+        PhoneNumberOfPersonString = PhoneNumberOfPerson.getText().toString();
 
         mProfileImage = findViewById(R.id.profileImage);
 
-        mRadioGroup = findViewById(R.id.Gender);
-        FemaleRadioButton = (RadioButton) findViewById(R.id.maleradiobutton);
-        MaleRadioButton = (RadioButton) findViewById(R.id.maleradiobutton);
+        genderspinnerradiogroup = findViewById(R.id.genderspinnerradiogroup);
+        FemaleRadioButton = (RadioButton) findViewById(R.id.FemaleMaleRadiobutton);
+        MaleRadiobutton = (RadioButton) findViewById(R.id.MaleRadiobutton);
+
+
+         agespinner = (Spinner)findViewById(R.id.agespinner);
+         NationalIDofEmergencyPerson = (EditText)findViewById(R.id.NationalIDofEmergencyPerson);
+         countryspinner = (Spinner)findViewById(R.id.countryspinner);
+         saveinformationhere = (Button)findViewById(R.id.saveinformationhere);
+         movetonext = (ImageButton)findViewById(R.id.movetonext);
+         homebutton = (ImageButton)findViewById(R.id.homebutton);
+        suggestionsbutton = (ImageButton)findViewById(R.id.suggestionsbutton);
+               services = (ImageButton)findViewById(R.id.services);
+          expectedshipping = (ImageButton)findViewById(R.id.expectedshipping);
+               adminprofile = (ImageButton)findViewById(R.id.adminprofile);
+
+
 
         mBack = findViewById(R.id.back);
         mConfirm = findViewById(R.id.confirm);
-        mySpinner = (Spinner) findViewById(R.id.agespinner);
         mAuth = FirebaseAuth.getInstance();
 
         FirebaseUser user = mAuth.getCurrentUser();
@@ -125,18 +160,18 @@ public class PersonalInformationForm extends AppCompatActivity {
 
             mAdminTraderDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(traderID);
             getUserInfo();
-
+                 // SET THE AGE ADAPTER
             ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PersonalInformationForm.this,
                     android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.agespinner));
             myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            mySpinner.setAdapter(myAdapter);
+            agespinner.setAdapter(myAdapter);
 
-            mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            agespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
 
                 @Override
                 public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                    text = mySpinner.getSelectedItem().toString();
+                    agetext = agespinner.getSelectedItem().toString();
                     getUserInfo();
                 }
 
@@ -147,6 +182,28 @@ public class PersonalInformationForm extends AppCompatActivity {
             });
 
         }
+
+        // SET THE COUNTRY ADAPTER
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PersonalInformationForm.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.countryspinner));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countryspinner.setAdapter(myAdapter);
+
+        countryspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                countrytext = countryspinner.getSelectedItem().toString();
+                getUserInfo();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
 
 
